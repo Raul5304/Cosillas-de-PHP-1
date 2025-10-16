d05a3b2817eba0da02d0c43884d4e44796cd1f77
Raul5304 <raullecinena@iesrioarba.es>
2025-10-16 11:52:49 +0200

    .

---
+++ Analizador de Texto/u02_cp02.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Analizador de texto</title>
+</head>
+<body>
+    <?php
+
+    $texto = "Gotham City es una metrópolis oscura, gótica y corrupta, situada en el corazón de los Estados Unidos. Desde sus orígenes, ha sido un reflejo distorsionado de la sociedad 
+    moderna: un lugar donde la desigualdad, el crimen y la desesperanza conviven bajo un cielo permanentemente cubierto de sombras. En sus calles, 
+    el miedo se respira como el humo de sus fábricas, y la justicia parece siempre ir un paso por detrás del mal.
+    En medio de este caos nació Bruce Wayne, el heredero de una de las familias más poderosas y respetadas de la ciudad. Siendo niño, fue testigo del asesinato de sus padres, 
+    Thomas y Martha Wayne, a manos de un ladrón común. Ese trauma marcó su destino para siempre. A partir de entonces, 
+    juró dedicar su vida a luchar contra el crimen y a evitar que otros sufrieran lo que él sufrió.
+    Durante años, Bruce se entrenó en cuerpo y mente: aprendió artes marciales, criminología, psicología y las más avanzadas tecnologías. 
+    Cuando estuvo preparado, adoptó una identidad que inspirara miedo en el corazón de los criminales: Batman, el Caballero Oscuro. Su símbolo, un murciélago, 
+    representa el terror que una vez lo dominó y que ahora utiliza como arma.
+    Desde su base en la cueva bajo la mansión Wayne, Batman libra una guerra constante contra el mal que corrompe Gotham. 
+    Se enfrenta a enemigos icónicos como el Joker, un payaso psicópata que personifica el caos; el Pingüino, un mafioso elegante y cruel; 
+    o Dos Caras, un fiscal caído que encarna la dualidad entre el bien y el mal.
+    A pesar de su fuerza y determinación, Batman no posee superpoderes. Su verdadera arma es su intelecto, su disciplina y su inquebrantable sentido de la justicia. 
+    Con la ayuda del comisario Gordon, Alfred Pennyworth y, en ocasiones, Robin, intenta mantener viva una chispa de esperanza en una ciudad que parece condenada.
+    Gotham es tanto su hogar como su prisión, una urbe que lo necesita tanto como lo teme. En el fondo, la historia de Batman y Gotham es la historia de una lucha eterna: 
+    la del hombre contra sus propios demonios, la del bien resistiendo, una noche más, frente a la oscuridad."
+
+
+    ?>
+</body>
+</html>
---
+++ Analizador de Texto/u02_cp02_funciones.php
@@ -0,0 +1,6 @@
+<?php
+
+$ignoredwords = ["a, ante, bajo, cabe, con, contra, de, desde, en, entre, hacia, hasta, para, por, según, sin, so, sobre, tras, 
+                y, e, ni, pero, porque, aunque, sin embargo, es decir, o, u, más, el, la, un, una, unos, unas, los, las"]
+
+?>
